300fd5b797b410198b5c58b7f9fa3df04e70c590
lucasfloriani <lucas.floriane@gmail.com>
2019-04-21 22:09:14 -0300

    Create RegisterPage component

---
+++ src/components/pages/RegisterPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, FullPageTemplate, UserRegisterForm } from 'components'
+
+const RegisterPage = () => (
+  <FullPageTemplate style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}>
+    <Container align="center" windowHeight>
+      <UserRegisterForm />
+    </Container>
+  </FullPageTemplate>
+)
+
+export default RegisterPage
