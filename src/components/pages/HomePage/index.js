28e0ed72192d65879a1a1c943b75b13535b4d751
lucasfloriani <lucas.floriane@gmail.com>
2019-04-21 22:09:04 -0300

    Change HomePage to user LoginForm

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Container, FullPageTemplate } from 'components'
+import { Container, FullPageTemplate, LoginForm } from 'components'
 
 const HomePage = () => (
   <FullPageTemplate style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}>
     <Container align="center" windowHeight>
-      Homepage
+      <LoginForm />
     </Container>
   </FullPageTemplate>
 )
