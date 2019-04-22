4ceca265aad91c1257664812491db7105549d871
lucasfloriani <lucas.floriane@gmail.com>
2019-04-21 22:04:29 -0300

    Add boilerplate code to GasStationHomePage

---
+++ src/components/pages/GasStationHomePage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, FullPageTemplate } from 'components'
+
+const GasStationHomePage = () => (
+  <FullPageTemplate style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}>
+    <Container align="center" windowHeight>
+      Homepage do gas station
+    </Container>
+  </FullPageTemplate>
+)
+
+export default GasStationHomePage
