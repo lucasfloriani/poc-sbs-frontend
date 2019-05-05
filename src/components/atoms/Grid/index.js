e3d394563963e2942a53d2f5a5b64370c5d7ed98
lucasfloriani <lucas.floriane@gmail.com>
2019-05-05 19:58:53 -0300

    Add more props to Grid component

--- src/components/atoms/Grid/index.js
+++ src/components/atoms/Grid/index.js
@@ -1,19 +1,25 @@
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const Grid = styled.div`
   display: grid;
   grid-gap: ${({ gap }) => gap};
-  grid-column: ${({ column }) => column};
+  grid-template-columns: ${({ column }) => column};
+  ${({ valign }) => valign && css`align-items: ${valign};`}
+  ${({ columnLength }) => columnLength && css`grid-column: ${columnLength};`}
+  ${({ rows }) => rows && css`grid-template-rows: ${rows}`};
+  width: 100%;
 `
 
 Grid.propTypes = {
   column: PropTypes.any,
+  columnLength: PropTypes.any,
   gap: PropTypes.string,
+  rows: PropTypes.any,
 }
 
 Grid.defaultProps = {
-  column: 1,
+  column: '1fr',
   gap: '15px',
 }
 
