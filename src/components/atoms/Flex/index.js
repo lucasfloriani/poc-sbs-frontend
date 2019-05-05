6bb9b12e5b9a5277fbb52f9bae1f1e9a6b625ede
lucasfloriani <lucas.floriane@gmail.com>
2019-05-05 19:58:31 -0300

    Add margin and width props to Flex component

--- src/components/atoms/Flex/index.js
+++ src/components/atoms/Flex/index.js
@@ -1,21 +1,27 @@
 import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
+import { prop } from 'styled-tools'
 
 const Flex = styled.div`
   display: flex;
+  ${({ margin }) => css`margin: ${margin};`}
   ${({ halign }) => css`justify-content: ${halign};`}
   ${({ valign }) => css`align-items: ${valign};`}
   flex-direction: ${({ flow }) => flow};
+  width: ${prop('width')};
 `
 
 Flex.propTypes = {
   flow: PropTypes.string,
   halign: PropTypes.string,
+  margin: PropTypes.string,
   valign: PropTypes.string,
+  width: PropTypes.string,
 }
 
 Flex.defaultProps = {
   flow: 'row',
+  width: '100%',
 }
 
 export default Flex
