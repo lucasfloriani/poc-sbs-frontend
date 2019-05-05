db08fc11fdd4b10fd59673c4f0eedd3fc221408b
lucasfloriani <lucas.floriane@gmail.com>
2019-05-05 19:59:51 -0300

    Create Rating component

---
+++ src/components/atoms/Rating/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+// import styled from 'styled-components'
+import StarRatings from 'react-star-ratings'
+import { Flex, Paragraph } from 'components'
+
+const Rating = ({
+  labelTitle, name, onChange, value,
+}) => (
+  <Flex flow="column">
+    <Paragraph color={{ type: 'grayscale', position: 0 }}>{labelTitle}</Paragraph>
+    <StarRatings
+      rating={value}
+      starRatedColor="#193336"
+      starHoverColor="#193336"
+      changeRating={onChange}
+      numberOfStars={5}
+      starDimension="30px"
+      starSpacing="3px"
+      name={name}
+    />
+  </Flex>
+)
+
+Rating.propTypes = {
+  labelTitle: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.number.isRequired,
+}
+
+export default Rating
