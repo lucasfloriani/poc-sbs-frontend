9292c8fef9d7bc5f4e7aa9c5f0c28ba3179b287c
lucasfloriani <lucas.floriane@gmail.com>
2019-05-07 01:51:01 -0300

    Add edit form in AdminEditGasStations component

--- src/components/pages/AdminEditGasStations/index.js
+++ src/components/pages/AdminEditGasStations/index.js
@@ -1,27 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import {
   AdminFooter,
   AdminMenu,
   Container,
   FullPageTemplate,
-  FilterGasStation,
-  Grid,
-  ListGasStations,
+  UpdateGasStationForm,
 } from 'components'
 
-const AdminEditGasStations = () => (
+const AdminEditGasStations = ({ match }) => (
   <FullPageTemplate
     header={<AdminMenu />}
     footer={<AdminFooter />}
     style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}
   >
     <Container align="center">
-      <Grid>
-        <FilterGasStation />
-        <ListGasStations actions={['edit']} />
-      </Grid>
+      <UpdateGasStationForm gasStationID={match.params.gasStationID} />
     </Container>
   </FullPageTemplate>
 )
+
+AdminEditGasStations.propTypes = {
+  match: PropTypes.any,
+}
 
 export default AdminEditGasStations
