0ccea880be04b687788f860f9f6f96825e25f078
lucasfloriani <lucas.floriane@gmail.com>
2019-05-07 01:50:39 -0300

    Add menu and footer from GasStation área

--- src/components/pages/GasStationHomePage/index.js
+++ src/components/pages/GasStationHomePage/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
-import { Container, FullPageTemplate } from 'components'
+import {
+  Container,
+  FullPageTemplate,
+  GasStationMenu,
+  GasStationFooter,
+} from 'components'
 
 const GasStationHomePage = () => (
-  <FullPageTemplate style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}>
+  <FullPageTemplate
+    header={<GasStationMenu />}
+    footer={<GasStationFooter />}
+    style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}
+  >
     <Container align="center" windowHeight>
       Homepage do gas station
     </Container>
