e0100d72ab02b0cf2cb4d7ae0886d81b8fc5decd
lucasfloriani <lucas.floriane@gmail.com>
2019-05-13 12:52:02 -0300

    Add RatingRender component

---
+++ src/components/molecules/RatingRender/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { CreateRatingForm, UpdateRatingForm, Modal } from 'components'
+
+const RatingRender = ({
+  children,
+  gasStationID,
+  ratings,
+  userID,
+}) => {
+  const [open, setOpen] = useState(false)
+  const toggleModal = () => setOpen(!open)
+  const userRating = ratings.find(rating => rating.user_id.toString() === userID)
+
+  return (
+    <>
+      {children(!!userRating, toggleModal)}
+      <Modal open={open} close={toggleModal}>
+        {userRating ? (
+          <UpdateRatingForm
+            initialValues={{
+              rating: userRating ? userRating.rating : 0,
+              id: userRating ? userRating.id : '',
+            }}
+            toggleModal={toggleModal}
+          />
+        ) : (
+          <CreateRatingForm
+            initialValues={{ gas_station_id: gasStationID, rating: 0 }}
+            toggleModal={toggleModal}
+          />
+        )}
+      </Modal>
+    </>
+  )
+}
+
+RatingRender.propTypes = {
+  children: PropTypes.func.isRequired,
+  gasStationID: PropTypes.string.isRequired,
+  ratings: PropTypes.array.isRequired,
+  userID: PropTypes.string.isRequired,
+}
+
+const mapStateToProps = ({ auth: { user: { id } } }) => ({ userID: id })
+
+export default connect(mapStateToProps)(RatingRender)
