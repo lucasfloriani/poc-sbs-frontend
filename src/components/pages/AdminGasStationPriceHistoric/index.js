4aa49a50c7094725f957b0cc875b1684b4d8ff1c
lucasfloriani <lucas.floriane@gmail.com>
2019-05-14 22:36:22 -0300

    Add code from AdminGasStationPriceHistoric page

---
+++ src/components/pages/AdminGasStationPriceHistoric/index.js
@@ -0,0 +1,85 @@
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Creators as GasStationActions } from '@store/ducks/gasStation'
+import {
+  AdminFooter,
+  AdminMenu,
+  Block,
+  Container,
+  ContentLoader,
+  FullPageTemplate,
+  Grid,
+  Heading,
+  Paragraph,
+  PriceFuelHistoryCard,
+} from 'components'
+
+const AdminGasStationPriceHistoric = ({
+  gasStation, getGasStationRequest, isFetching, match,
+}) => {
+  useEffect(() => { getGasStationRequest(match.params.gasStationID) }, [])
+  if (isFetching) return (<ContentLoader />)
+
+  return (
+    <FullPageTemplate
+      header={<AdminMenu />}
+      footer={<AdminFooter />}
+      style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}
+    >
+      <Container align="center">
+        <Grid>
+          <Block>
+            <Heading
+              color={{ type: 'grayscale', position: 4 }}
+              hoverColor={{ type: 'grayscale', position: 4 }}
+            >
+              {gasStation.fantasy_name}
+            </Heading>
+          </Block>
+          {gasStation.priceFuelHistories && (
+            <Grid column="1fr 1fr 1fr">
+              <Block backgroundColor={{ type: 'primary', position: 4 }}>
+                <Paragraph align="center" fontSize="medium">Criar</Paragraph>
+              </Block>
+              <Block backgroundColor={{ type: 'alert', position: 2 }}>
+                <Paragraph align="center" fontSize="medium">Atualização</Paragraph>
+              </Block>
+              <Block backgroundColor={{ type: 'error', position: 3 }}>
+                <Paragraph align="center" fontSize="medium">Remoção</Paragraph>
+              </Block>
+            </Grid>
+          )}
+          {gasStation.priceFuelHistories && gasStation.priceFuelHistories.map(price => (
+            <PriceFuelHistoryCard
+              key={price.updated_at}
+              createdAt={price.created_at}
+              fuelTypeName={price.fuelType.name}
+              paymentTypeName={price.paymentType.name}
+              price={price.price}
+              type={price.type}
+            />
+          ))}
+          {gasStation.priceFuelHistories && !gasStation.priceFuelHistories.length && (
+            <Block>
+              <Paragraph color={{ type: 'grayscale', position: 4 }}>Não foi encontrado nenhum histórico</Paragraph>
+            </Block>
+          )}
+        </Grid>
+      </Container>
+    </FullPageTemplate>
+  )
+}
+
+AdminGasStationPriceHistoric.propTypes = {
+  gasStation: PropTypes.object.isRequired,
+  getGasStationRequest: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  match: PropTypes.any.isRequired,
+}
+
+const mapStateToProps = ({ gasStation: { gasStation, isFetching } }) => ({ gasStation, isFetching })
+const mapDispatchToProps = dispatch => bindActionCreators(GasStationActions, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdminGasStationPriceHistoric)
