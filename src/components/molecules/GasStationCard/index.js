93a59f08d44fb2af599714940a2b8cb4c5e99229
lucasfloriani <lucas.floriane@gmail.com>
2019-05-19 17:07:59 -0300

    Add priceFuel logc in GasStationCard

--- src/components/molecules/GasStationCard/index.js
+++ src/components/molecules/GasStationCard/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Creators as GasStationActions } from '@store/ducks/gasStation'
+import paymentType from '@enums/paymentType'
 import {
   Badge,
   BadgeIcon,
@@ -28,64 +29,91 @@ const GasStationCard = ({
   complaints,
   complement,
   fantasyName,
+  fuelTypeName,
   geoLocation,
   id,
   neighborhood,
+  priceFuels,
   ratings,
   setGasStationLocation,
   stateName,
-}) => (
-  <Card padding="medium">
-    <Grid valign="flex-start" column="1fr auto">
-      <Flex flow="column">
-        <Heading margin="0">{fantasyName}</Heading>
-        <Heading margin="0 0 8px">{cnpj}</Heading>
-        <Paragraph>{`${address} ${complement} - ${neighborhood}`}</Paragraph>
-        <Paragraph>{`${stateName}/${cityName}, CEP ${cep}`}</Paragraph>
-      </Flex>
-      <Flex>
-        {actions.includes('edit') && <Link to={`/admin/gas-stations/${id}`}><BadgeIcon icon="edit" /></Link>}
-        {actions.includes('rating') && (
-          <RatingRender ratings={ratings} gasStationID={`${id}`}>
-            {(userRating, toggleModal) => (
-              <Badge>
-                <Icon
-                  cursor="pointer"
-                  icon="star"
-                  color={userRating ? { type: 'alert', position: 0 } : { type: 'primary', position: 0 }}
-                  hoverColor={userRating ? { type: 'alert', position: 0 } : { type: 'primary', position: 0 }}
-                  onClick={toggleModal}
-                />
-              </Badge>
-            )}
-          </RatingRender>
-        )}
-        {actions.includes('alert') && (
-          <ComplaintRender complaints={complaints} gasStationID={`${id}`}>
-            {(userComplaint, toggleModal) => (
-              <Badge>
-                <Icon
-                  cursor="pointer"
-                  icon="alert"
-                  color={userComplaint ? { type: 'primary', position: 4 } : { type: 'primary', position: 0 }}
-                  hoverColor={userComplaint ? { type: 'primary', position: 4 } : { type: 'primary', position: 0 }}
-                  onClick={toggleModal}
-                />
-              </Badge>
-            )}
-          </ComplaintRender>
-        )}
-        {actions.includes('bookmark') && <BookmarkBadge bookmarks={bookmarks} gasStationID={`${id}`} />}
-        {actions.includes('navigation') && (
-          <BadgeIcon
-            icon="navigation"
-            onClick={() => setGasStationLocation({ location: geoLocation, name: fantasyName })}
-          />
-        )}
-      </Flex>
-    </Grid>
-  </Card>
-)
+}) => {
+  const initialValue = Object.values(paymentType).reduce((acc, type) => ({ ...acc, [type]: '-----' }), {})
+  const onlySelectedFuelType = priceFuels.filter(priceFuel => priceFuel.fuelType.name === fuelTypeName)
+  const priceFuelValues = onlySelectedFuelType.reduce((acc, priceFuel) => {
+    return Object.values(paymentType).includes(priceFuel.paymentType.name)
+      ? { ...acc, [priceFuel.paymentType.name]: priceFuel.price }
+      : acc
+  }, initialValue)
+
+  return (
+    <Card padding="medium">
+      <Grid valign="flex-start" column="1fr auto">
+        <Flex flow="column">
+          <Heading margin="0">{fantasyName}</Heading>
+          <Heading margin="0 0 8px">{cnpj}</Heading>
+          <Paragraph>{`${address} ${complement} - ${neighborhood}`}</Paragraph>
+          <Paragraph>{`${stateName}/${cityName}, CEP ${cep}`}</Paragraph>
+        </Flex>
+        <Flex>
+          {actions.includes('edit') && <Link to={`/admin/gas-stations/${id}`}><BadgeIcon icon="edit" /></Link>}
+          {actions.includes('rating') && (
+            <RatingRender ratings={ratings} gasStationID={`${id}`}>
+              {(userRating, toggleModal) => (
+                <Badge>
+                  <Icon
+                    cursor="pointer"
+                    icon="star"
+                    color={userRating ? { type: 'alert', position: 0 } : { type: 'primary', position: 0 }}
+                    hoverColor={userRating ? { type: 'alert', position: 0 } : { type: 'primary', position: 0 }}
+                    onClick={toggleModal}
+                  />
+                </Badge>
+              )}
+            </RatingRender>
+          )}
+          {actions.includes('alert') && (
+            <ComplaintRender complaints={complaints} gasStationID={`${id}`}>
+              {(userComplaint, toggleModal) => (
+                <Badge>
+                  <Icon
+                    cursor="pointer"
+                    icon="alert"
+                    color={userComplaint ? { type: 'primary', position: 4 } : { type: 'primary', position: 0 }}
+                    hoverColor={userComplaint ? { type: 'primary', position: 4 } : { type: 'primary', position: 0 }}
+                    onClick={toggleModal}
+                  />
+                </Badge>
+              )}
+            </ComplaintRender>
+          )}
+          {actions.includes('bookmark') && <BookmarkBadge bookmarks={bookmarks} gasStationID={`${id}`} />}
+          {actions.includes('navigation') && (
+            <BadgeIcon
+              icon="navigation"
+              onClick={() => setGasStationLocation({ location: geoLocation, name: fantasyName })}
+            />
+          )}
+        </Flex>
+      </Grid>
+      {fuelTypeName && (
+        <Grid column="1fr 1fr 1fr" gap="10px" style={{ marginTop: '20px' }}>
+          <Paragraph fontSize="large" style={{ gridColumn: '1 / -1' }}>{fuelTypeName}</Paragraph>
+          {
+            Object.entries(priceFuelValues).map(([key, value]) => (
+              <Card backgroundColor={{ type: 'grayscale', position: 1 }} key={key} padding="small">
+                <Paragraph align="center" color={{ type: 'grayscale', position: 4 }} fontSize="medium">
+                  {value !== '-----' ? `R$ ${value.replace('.', ',')}` : value}
+                </Paragraph>
+                <Paragraph align="center" color={{ type: 'grayscale', position: 4 }}>{key}</Paragraph>
+              </Card>
+            ))
+          }
+        </Grid>
+      )}
+    </Card>
+  )
+}
 
 GasStationCard.propTypes = {
   actions: PropTypes.array.isRequired,
@@ -97,12 +125,14 @@ GasStationCard.propTypes = {
   complaints: PropTypes.array.isRequired,
   complement: PropTypes.string.isRequired,
   fantasyName: PropTypes.string.isRequired,
+  fuelTypeName: PropTypes.string.isRequired,
   geoLocation: PropTypes.string.isRequired,
   id: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
   ]).isRequired,
   neighborhood: PropTypes.string.isRequired,
+  priceFuels: PropTypes.array.isRequired,
   ratings: PropTypes.array.isRequired,
   setGasStationLocation: PropTypes.func.isRequired,
   stateName: PropTypes.string.isRequired,
@@ -112,6 +142,7 @@ GasStationCard.defaultProps = {
   actions: ['alert', 'bookmark', 'navigation', 'rating'],
 }
 
+const mapStateToProps = ({ auth: { fuelTypeName } }) => ({ fuelTypeName })
 const mapDispatchToProps = dispatch => bindActionCreators(GasStationActions, dispatch)
 
-export default connect(null, mapDispatchToProps)(GasStationCard)
+export default connect(mapStateToProps, mapDispatchToProps)(GasStationCard)
