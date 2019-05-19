26587627e8a832d5096c81a0b21bc09065d11932
lucasfloriani <lucas.floriane@gmail.com>
2019-05-19 19:52:27 -0300

    Remove unused helpers

--- src/helpers/array.js
+++
@@ -1,3 +0,0 @@
-export const onlyTrufyValues = list => list.filter(item => item)
-
-export const listToString = list => list.map(item => item.toString())
--- src/helpers/text.js
+++
@@ -1,1 +0,0 @@
-export const limitText = (text, qty) => text.length > qty ? `${text.slice(0, qty)}...` : text
