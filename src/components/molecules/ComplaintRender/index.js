4b13543c818d6586dc5bf41ba4274ad2693cf897
lucasfloriani <lucas.floriane@gmail.com>
2019-08-18 19:46:39 -0300

    Remove update option in complaint render

--- src/components/molecules/ComplaintRender/index.js
+++ src/components/molecules/ComplaintRender/index.js
@@ -1,33 +1,21 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { CreateComplaintForm, Modal, UpdateComplaintForm } from 'components'
+import { CreateComplaintForm, Modal } from 'components'
 
 const ComplaintRender = ({
-  children, complaints, gasStationID, userID,
+  children, gasStationID,
 }) => {
   const [open, setOpen] = useState(false)
   const toggleModal = () => setOpen(!open)
-  const userComplaint = complaints.find(complaint => complaint.user_id.toString() === userID)
 
   return (
     <>
-      {children(!!userComplaint, toggleModal)}
+      {children(toggleModal)}
       <Modal open={open} close={toggleModal}>
-        {userComplaint ? (
-          <UpdateComplaintForm
-            initialValues={{
-              id: userComplaint ? `${userComplaint.id}` : '',
-              message: userComplaint ? `${userComplaint.message}` : '',
-            }}
-            toggleModal={toggleModal}
-          />
-        ) : (
-          <CreateComplaintForm
-            initialValues={{ gas_station_id: gasStationID, message: '' }}
-            toggleModal={toggleModal}
-          />
-        )}
+        <CreateComplaintForm
+          initialValues={{ gas_station_id: gasStationID, message: '' }}
+          toggleModal={toggleModal}
+        />
       </Modal>
     </>
   )
@@ -35,11 +23,7 @@ const ComplaintRender = ({
 
 ComplaintRender.propTypes = {
   children: PropTypes.func.isRequired,
-  complaints: PropTypes.array.isRequired,
   gasStationID: PropTypes.string.isRequired,
-  userID: PropTypes.string.isRequired,
 }
 
-const mapStateToProps = ({ auth: { user: { id } } }) => ({ userID: `${id}` })
-
-export default connect(mapStateToProps)(ComplaintRender)
+export default ComplaintRender
