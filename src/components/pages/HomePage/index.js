feb409ba6a05f741ba6926e24756533581fe2c37
lucasfloriani <lucas.floriane@gmail.com>
2019-08-18 19:57:16 -0300

    Refactor HomePage to access info about gas stations

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -1,12 +1,67 @@
-import React from 'react'
-import { Container, FullPageTemplate, LoginForm } from 'components'
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Creators as GasStationActions } from '@store/ducks/gasStation'
+import {
+  Container,
+  FullPageTemplate,
+  FilterGasStation,
+  Grid,
+  ListGasStations,
+  MapPins,
+  ScreenLoader,
+  UserFooter,
+  UserMenu,
+} from 'components'
 
-const HomePage = () => (
-  <FullPageTemplate mainPadding="0" style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}>
-    <Container align="center" windowHeight>
-      <LoginForm />
-    </Container>
-  </FullPageTemplate>
-)
+const HomePage = ({ gasStations, gasStationsRequest, isFetching }) => {
+  useEffect(() => {
+    const filterValues = {
+      name: '',
+      state: '',
+      city: '',
+      orderType: '',
+      paymentType: '',
+      fuelType: '',
+      minPrice: '',
+      maxPrice: '',
+      rating: 0,
+    }
+    gasStationsRequest(filterValues)
+  }, [])
+  if (gasStations.length === 0 || isFetching) return (<ScreenLoader />)
 
-export default HomePage
+  const gasStationsPins = gasStations.map(gasStation => ({
+    location: gasStation.geo_location,
+    name: gasStation.fantasy_name,
+  }))
+
+  return (
+    <FullPageTemplate
+      header={<UserMenu />}
+      footer={<UserFooter />}
+      style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}
+    >
+      <Container align="center">
+        <Grid>
+          <MapPins pins={gasStationsPins} />
+          <FilterGasStation />
+          <ListGasStations />
+        </Grid>
+      </Container>
+    </FullPageTemplate>
+  )
+}
+
+HomePage.propTypes = {
+  gasStations: PropTypes.array,
+  gasStationsRequest: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+}
+
+
+const mapStateToProps = ({ gasStation: { gasStations, isFetching } }) => ({ gasStations, isFetching })
+const mapDispatchToProps = dispatch => bindActionCreators(GasStationActions, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
--- src/components/pages/HomePage/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import HomePage from '.'
-
-describe('<HomePage />', () => {
-  it('renders', () => {
-    shallow(<HomePage />)
-  })
-})
