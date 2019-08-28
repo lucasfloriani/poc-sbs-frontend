130951ce125231f2ca4cb20469cac27838d5e78b
lucasfloriani <lucas.floriane@gmail.com>
2019-08-28 20:57:41 -0300

    Add relatories actions in page

--- src/components/pages/AdminRelatories/index.js
+++ src/components/pages/AdminRelatories/index.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Creators as ComplaintActions } from '@store/ducks/complaint'
+import { Creators as GasStationActions } from '@store/ducks/gasStation'
+import { Creators as PriceFuelActions } from '@store/ducks/priceFuel'
 import {
   AdminFooter,
   AdminMenu,
@@ -12,26 +14,36 @@ import {
   Grid,
 } from 'components'
 
-const AdminRelatories = ({ complaintRelatoryRequest }) => {
-  return (
-    <FullPageTemplate
-      header={<AdminMenu />}
-      footer={<AdminFooter />}
-      style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}
-    >
-      <Container align="center">
-        <Grid>
-          <Button onClick={() => complaintRelatoryRequest()}>Baixar denúncias</Button>
-        </Grid>
-      </Container>
-    </FullPageTemplate>
-  )
-}
+const AdminRelatories = ({
+  complaintRelatoryRequest,
+  gasStationRelatoryRequest,
+  priceFuelHistoryRelatoryRequest,
+}) => (
+  <FullPageTemplate
+    header={<AdminMenu />}
+    footer={<AdminFooter />}
+    style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}
+  >
+    <Container align="center">
+      <Grid>
+        <Button onClick={() => complaintRelatoryRequest()}>Baixar denúncias</Button>
+        <Button onClick={() => gasStationRelatoryRequest()}>Baixar postos</Button>
+        <Button onClick={() => priceFuelHistoryRelatoryRequest()}>Baixar históricos de preços</Button>
+      </Grid>
+    </Container>
+  </FullPageTemplate>
+)
 
 AdminRelatories.propTypes = {
   complaintRelatoryRequest: PropTypes.func.isRequired,
+  gasStationRelatoryRequest: PropTypes.func.isRequired,
+  priceFuelHistoryRelatoryRequest: PropTypes.func.isRequired,
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators(ComplaintActions, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({
+  ...ComplaintActions,
+  ...GasStationActions,
+  ...PriceFuelActions,
+}, dispatch)
 
 export default connect(null, mapDispatchToProps)(AdminRelatories)
