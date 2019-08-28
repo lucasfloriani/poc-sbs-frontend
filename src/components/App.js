75bf18d4ba71b66710f7ecfd39498523c5e0c5c8
lucasfloriani <lucas.floriane@gmail.com>
2019-08-28 00:30:00 -0300

    Add pages to app

--- src/components/App.js
+++ src/components/App.js
@@ -34,16 +34,20 @@ const LoginPage = lazy(() => import('@pages/LoginPage/index'))
 const RegisterPage = lazy(() => import('@pages/RegisterPage/index'))
 const NotFoundPage = lazy(() => import('@pages/NotFoundPage/index'))
 // User pages
+const UserAboutUs = lazy(() => import('@pages/UserAboutUs/index'))
 const UserBookmarks = lazy(() => import('@pages/UserBookmarks/index'))
 const UserRatings = lazy(() => import('@pages/UserRatings/index'))
 const UserEdit = lazy(() => import('@pages/UserEdit/index'))
 // Gas Station pages
+const GasStationAboutUs = lazy(() => import('@pages/GasStationAboutUs/index'))
 const GasStationHomePage = lazy(() => import('@pages/GasStationHomePage/index'))
 const GasStationCreatePriceFuel = lazy(() => import('@pages/GasStationCreatePriceFuel/index'))
 const GasStationEditPriceFuel = lazy(() => import('@pages/GasStationEditPriceFuel/index'))
 // Admin pages
+const AdminAboutUs = lazy(() => import('@pages/AdminAboutUs/index'))
 const AdminListGasStations = lazy(() => import('@pages/AdminListGasStations/index'))
 const AdminComplaints = lazy(() => import('@pages/AdminComplaints/index'))
+const AdminRelatories = lazy(() => import('@pages/AdminRelatories/index'))
 const AdminCreateGasStations = lazy(() => import('@pages/AdminCreateGasStations/index'))
 const AdminEditGasStations = lazy(() => import('@pages/AdminEditGasStations/index'))
 const AdminGasStationPriceHistoric = lazy(() => import('@pages/AdminGasStationPriceHistoric/index'))
@@ -56,6 +60,7 @@ const App = () => (
       <Suspense fallback={<ScreenLoader />}>
         <Switch>
           <Route path="/" exact component={props => <HomePage {...props} />} />
+          <Route path="/about-us" exact component={props => <UserAboutUs {...props} />} />
           <PublicRouter path="/login" exact component={props => <LoginPage {...props} />} />
           <PublicRouter path="/register" exact component={props => <RegisterPage {...props} />} />
 
@@ -64,14 +69,17 @@ const App = () => (
           <UserRouter path="/user/edit" exact component={props => <UserEdit {...props} />} />
 
           <GasStationRouter path="/gas-station" exact component={props => <GasStationHomePage {...props} />} />
+          <GasStationRouter path="/gas-station/about-us" exact component={props => <GasStationAboutUs {...props} />} />
           <GasStationRouter path="/gas-station/price-fuel" exact component={props => <GasStationCreatePriceFuel {...props} />} />
           <GasStationRouter path="/gas-station/price-fuel/:priceFuelID" exact component={props => <GasStationEditPriceFuel {...props} />} />
 
           <AdminRouter path="/admin" exact component={props => <AdminListGasStations {...props} />} />
+          <AdminRouter path="/admin/about-us" exact component={props => <AdminAboutUs {...props} />} />
           <AdminRouter path="/admin/complaints" exact component={props => <AdminComplaints {...props} />} />
           <AdminRouter path="/admin/gas-stations" exact component={props => <AdminCreateGasStations {...props} />} />
           <AdminRouter path="/admin/gas-stations/:gasStationID" exact component={props => <AdminEditGasStations {...props} />} />
           <AdminRouter path="/admin/gas-stations/:gasStationID/price-historic" exact component={props => <AdminGasStationPriceHistoric {...props} />} />
+          <AdminRouter path="/admin/relatories" exact component={props => <AdminRelatories {...props} />} />
 
           <Route path="*" component={props => <NotFoundPage {...props} />} />
         </Switch>
