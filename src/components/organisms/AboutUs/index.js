4617605f55e1061b44c844cee7c66b27591e0f97
lucasfloriani <lucas.floriane@gmail.com>
2019-08-28 00:28:23 -0300

    Create AboutUs component

---
+++ src/components/organisms/AboutUs/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+  Card,
+  Heading,
+  Paragraph,
+} from 'components'
+
+const AboutUs = () => (
+  <Card padding="small" style={{ width: '100%' }}>
+    <Heading margin="0 0 12px">Sobre nós</Heading>
+    <Paragraph fontSize="small">
+      Projeto desenvolvido pela parceria entre UDESC, Prefeitura Municipal e Procon de São Bento do Sul, o qual visa fornecer a população
+      uma forma mais eficiente de buscar preços de combustíveis dos postos da região.
+    </Paragraph>
+    <Paragraph fontSize="small" margin="10px 0 0">
+      O desenvolvimento foi realizado pelo setor de extensão da UDESC, onde os alunos Lucas Alexander Floriani, Karina Neuburger e
+      Jeferson Venceslau Marcos desenvolveram o mesmo.
+    </Paragraph>
+  </Card>
+)
+
+export default AboutUs
