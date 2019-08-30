00f042ef5f8725da8cacddc6bee8309ddb4411d3
lucasfloriani <lucas.floriane@gmail.com>
2019-08-30 16:11:35 -0300

    Create ImageInput component

---
+++ src/components/molecules/ImageInput/index.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import Dropzone from 'react-dropzone'
+import { Paragraph } from 'components' // Loader
+import { readAsArrayBuffer } from 'promise-file-reader'
+import { getInfoFileByBlob } from '@helpers/file' // getTextFromArrayBuffer
+import {
+  Wrapper,
+  FieldWrapper,
+  Message,
+  StyledImage,
+  Error,
+} from './style'
+
+const ImageInput = ({
+  accept, error, height, image, onDropCallback,
+}) => {
+  const [imageData, setImageData] = useState({ content: '', name: '' })
+  useEffect(() => {
+    (async () => {
+      if (!image) {
+        setImageData({ content: '', name: '' })
+        return
+      }
+
+      const { content, name } = await getInfoFileByBlob(image)
+      setImageData({ content, name })
+    })()
+  }, [image])
+
+  return (
+    <Wrapper>
+      <Dropzone
+        accept={accept}
+        onDrop={async (acceptedFiles) => {
+          if (acceptedFiles.length === 0) return
+          const photo = acceptedFiles[0]
+          const readerResult = await readAsArrayBuffer(photo)
+          const blob = new Blob([readerResult], { type: photo.type })
+          onDropCallback(blob)
+        }}
+      >
+        {({ getRootProps, getInputProps, isDragActive }) => {
+          const message = isDragActive ? 'Solte o arquivo aqui...' : 'Solte um arquivo aqui ou clique para selecionar um.'
+          return (
+            <div {...getRootProps()}>
+              <FieldWrapper height={height} isDragActive={isDragActive}>
+                <input {...getInputProps()} />
+                <StyledImage maxHeight={height} zoom={1.05} alt={imageData.name} src={imageData.content} />
+                {!imageData.content && <Message isDragActive={isDragActive}>{message}</Message>}
+              </FieldWrapper>
+            </div>
+          )
+        }}
+      </Dropzone>
+      {error && <Error>{Object.entries(error).map(([key, value]) => <Paragraph key={key}>{value}</Paragraph>)}</Error>}
+    </Wrapper>
+  )
+}
+
+ImageInput.propTypes = {
+  accept: PropTypes.string,
+  error: PropTypes.object,
+  height: PropTypes.string,
+  image: PropTypes.any,
+  onDropCallback: PropTypes.func.isRequired,
+}
+
+ImageInput.defaultProps = {
+  accept: 'image/*',
+  height: '300px',
+}
+
+export default ImageInput
---
+++ src/components/molecules/ImageInput/style.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components'
+import { ifProp, prop } from 'styled-tools'
+import { font, key, palette } from 'styled-theme'
+import { Image, Paragraph } from 'components'
+
+export const Wrapper = styled.div`
+  align-items: center;
+  grid-column: 1 / -1;
+  justify-content: center;
+  width: 100%;
+`
+
+export const FieldWrapper = styled(({ isDragActive, height, ...props }) => <div {...props} />)`
+  align-items: center;
+  background-color: ${ifProp('isDragActive', palette('primary', 0), palette('grayscale', 4))};
+  border-radius: 3px;
+  box-shadow: ${key(['shadows', 'extraSmall'])};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: ${prop('height')};
+  padding: 10px 12px;
+  outline: none;
+  overflow: hidden;
+  transition: .6s background-color ${key(['cubicBezier', 'standard'])}, .3s box-shadow ${key(['cubicBezier', 'standard'])};
+
+  &:hover {
+    box-shadow: ${key(['shadows', 'medium'])};
+  }
+`
+
+export const Message = styled(({ isDragActive, ...props }) => <Paragraph {...props} />)`
+  color: ${ifProp('isDragActive', palette('grayscale', 4), palette('grayscale', 0))};
+  padding: 6px;
+  text-align: center;
+  transition: .6s color ${key(['cubicBezier', 'standard'])};
+`
+
+export const StyledImage = styled(Image)`
+  display: ${ifProp('src', 'block', 'none')};
+  max-width: 100%;
+  max-height: ${prop('maxHeight')};
+`
+
+export const Error = styled.div`
+  background-color: ${palette('danger', 2)};
+  border-left: 5px solid ${palette('danger', 0)};
+  border-radius: 0 0 3px 3px;
+  box-shadow: ${key(['shadows', 'extraSmall'])};
+  box-sizing: border-box;
+  color: ${palette('grayscale', 4)};
+  font-family: ${font('primary')};
+  font-size: .8em;
+  letter-spacing: 0.4px;
+  padding: 6px;
+`
