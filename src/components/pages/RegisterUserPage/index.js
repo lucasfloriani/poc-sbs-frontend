c5466472c0417d35d29a1e485eb649b6d0e24980
lucasfloriani <lucas.floriane@gmail.com>
2019-08-29 21:12:18 -0300

    Rename component

--- src/components/pages/RegisterUserPage/index.js
+++ src/components/pages/RegisterUserPage/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Container, FullPageTemplate, UserRegisterForm } from 'components'
 
-const RegisterPage = () => (
-  <FullPageTemplate mainPadding="0" style={{ backgroundImage: 'linear-gradient(to top, #cfd9df 0%, #e2ebf0 100%)' }}>
+const RegisterUserPage = () => (
+  <FullPageTemplate mainPadding="0">
     <Container align="center" windowHeight>
       <UserRegisterForm />
     </Container>
   </FullPageTemplate>
 )
 
-export default RegisterPage
+export default RegisterUserPage
