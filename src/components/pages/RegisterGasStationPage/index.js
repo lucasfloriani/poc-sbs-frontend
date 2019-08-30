da5273c7e2afe05316901533fbb63d25f24fc449
lucasfloriani <lucas.floriane@gmail.com>
2019-08-29 21:12:37 -0300

    Create RegisterGasStationPage component

---
+++ src/components/pages/RegisterGasStationPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, FullPageTemplate, GasStationRegisterForm } from 'components'
+
+const RegisterGasStationPage = () => (
+  <FullPageTemplate mainPadding="0">
+    <Container align="center" windowHeight>
+      <GasStationRegisterForm />
+    </Container>
+  </FullPageTemplate>
+)
+
+export default RegisterGasStationPage
