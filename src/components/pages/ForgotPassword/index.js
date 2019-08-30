33e72e249584980872dfe32cb51d44441f90f8c2
lucasfloriani <lucas.floriane@gmail.com>
2019-08-29 23:39:48 -0300

    Create ForgotPassword component

---
+++ src/components/pages/ForgotPassword/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, FullPageTemplate, ForgotPasswordForm } from 'components'
+
+const ForgotPassword = () => (
+  <FullPageTemplate mainPadding="0">
+    <Container align="center" windowHeight>
+      <ForgotPasswordForm />
+    </Container>
+  </FullPageTemplate>
+)
+
+export default ForgotPassword
