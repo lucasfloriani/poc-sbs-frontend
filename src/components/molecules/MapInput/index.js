650ebf1084894df17e386dfc5ae1f33293d25975
lucasfloriani <lucas.floriane@gmail.com>
2019-12-22 21:42:23 -0300

    Correct MapInput component to change pin position when value changes

--- src/components/molecules/MapInput/index.js
+++ src/components/molecules/MapInput/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import Map from 'pigeon-maps'
 import Draggable from 'pigeon-draggable'
@@ -6,11 +6,16 @@ import mapPin from '@atoms/Icon/icons/mapPin.svg'
 import { Error, Grid } from 'components'
 
 const MapInput = ({
-  centerValue, error, setValue, value, zoomValue,
+  error, setValue, value, zoomValue,
 }) => {
-  const [center, setCenter] = useState(centerValue)
-  const [zoom, setZoom] = useState(zoomValue)
   const [dragAnchor, setDragAnchor] = useState(value)
+  const [center, setCenter] = useState(value)
+  const [zoom, setZoom] = useState(zoomValue)
+
+  useEffect(() => {
+    setDragAnchor(value)
+    setCenter(value)
+  }, [value])
 
   return (
     <Grid>
@@ -40,7 +45,6 @@ const MapInput = ({
 }
 
 MapInput.propTypes = {
-  centerValue: PropTypes.array.isRequired,
   error: PropTypes.string,
   setValue: PropTypes.func.isRequired,
   value: PropTypes.array.isRequired,
@@ -48,7 +52,6 @@ MapInput.propTypes = {
 }
 
 MapInput.defaultProps = {
-  centerValue: [-26.244383377008926, -49.384092876981356],
   value: [-26.244383377008926, -49.384092876981356],
   zoomValue: 16,
 }
