a637f4482194ac3943234418e43a04f24a78197a
lucasfloriani <lucas.floriane@gmail.com>
2019-12-26 00:26:25 -0300

    Refactior CitySelect to use hooks

--- src/components/molecules/CitySelect/index.js
+++ src/components/molecules/CitySelect/index.js
@@ -1,27 +1,22 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Creators as CityActions } from '@store/ducks/city'
 import Select from '@atoms/Select'
 
-const CitySelect = ({
-  cities, citiesRequest, stateID, ...props
-}) => {
-  useEffect(() => { stateID && citiesRequest(stateID) }, [citiesRequest, stateID])
+const CitySelect = ({ stateID, ...props }) => {
+  const dispatch = useDispatch()
+  useEffect(() => { stateID && dispatch(CityActions.citiesRequest(stateID)) }, [dispatch, stateID])
+  const cities = useSelector(store => store.city.cities)
+
   return <Select options={cities} {...props} />
 }
 
 CitySelect.propTypes = {
-  cities: PropTypes.array.isRequired,
-  citiesRequest: PropTypes.func.isRequired,
   stateID: PropTypes.oneOfType([
     PropTypes.number,
     PropTypes.string,
   ]).isRequired,
 }
 
-const mapStateToProps = ({ city: { cities, isFetching } }) => ({ cities, isFetching })
-const mapDispatchToProps = dispatch => bindActionCreators(CityActions, dispatch)
-
-export default connect(mapStateToProps, mapDispatchToProps)(CitySelect)
+export default CitySelect
