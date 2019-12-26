f0626f373048d8da2dc73062cc19145cdf384b62
lucasfloriani <lucas.floriane@gmail.com>
2019-12-26 00:26:54 -0300

    Refactior StateSelect to use hooks

--- src/components/molecules/StateSelect/index.js
+++ src/components/molecules/StateSelect/index.js
@@ -1,21 +1,14 @@
 import React, { useEffect } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Creators as StateActions } from '@store/ducks/state'
 import Select from '@atoms/Select'
 
-const StateSelect = ({ states, statesRequest, ...props }) => {
-  useEffect(() => { statesRequest() }, [statesRequest])
+const StateSelect = ({ ...props }) => {
+  const dispatch = useDispatch()
+  useEffect(() => { dispatch(StateActions.statesRequest()) }, [dispatch])
+  const states = useSelector(store => store.state.states)
+
   return <Select options={states} {...props} />
 }
 
-StateSelect.propTypes = {
-  states: PropTypes.array.isRequired,
-  statesRequest: PropTypes.func.isRequired,
-}
-
-const mapStateToProps = ({ state: { states, isFetching } }) => ({ states, isFetching })
-const mapDispatchToProps = dispatch => bindActionCreators(StateActions, dispatch)
-
-export default connect(mapStateToProps, mapDispatchToProps)(StateSelect)
+export default StateSelect
